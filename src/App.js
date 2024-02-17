2ee370708ab04afa220c7eee77f78f70a8cc1559
Pellegrino Piccolo <152791429+PellegrinoPiccolo@users.noreply.github.com>
2024-02-17 16:30:38 +0100

    home

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import ProjectCard from "./components/projectCard/ProjectCard";
 import './style.scss'
-import Logo from './media/logo.png'
-import { projects_card } from "./project_card";
-import Footer from "./components/footer/Footer";
+import Home from "./pages/Home";
+import React from 'react';
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import Footer from './components/footer/Footer';
 
 function App() {
+
+  const Layout = () =>{
+    return(
+      <>
+        <Outlet />
+        <Footer />
+      </>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+      ]
+    },
+  ]);
+  
   
   return (
-    <div className="home">
-      <div className="top_section">
-        <img src={Logo} alt="PellegrinoPiccolo"/>
-        <div className="text_section">
-          <h2 className="h_page">THIS IS ONLY MY PROJECTS COLLECTION</h2>
-          <h2 className="h_page">IF YOU WANT VISIT MY PORTFOLIO YOU CAN CLICK ON <a href="https://pellegrinopiccolo.github.io/" target="_blank">THIS LINK (pellegrinopiccolo.github.io)</a></h2>
-        </div>
-      </div>
-      <h1 className="center h_page">My Projects</h1>
-      <div className="card_container">
-        {projects_card.map(project=>(
-          <ProjectCard project={project}/>
-        ))}
-      </div>
-      <Footer />
-    </div>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ProjectCard from "../components/projectCard/ProjectCard";
+import '../style.scss'
+import Logo from '../media/logo.png'
+import { projects_card } from "../project_card";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="top_section">
+        <img src={Logo} alt="PellegrinoPiccolo"/>
+        <div className="text_section">
+          <h2 className="h_page">THIS IS ONLY MY PROJECTS COLLECTION</h2>
+          <h2 className="h_page">IF YOU WANT VISIT MY PORTFOLIO YOU CAN CLICK ON <a href="https://pellegrinopiccolo.github.io/" target="_blank">THIS LINK (pellegrinopiccolo.github.io)</a></h2>
+        </div>
+      </div>
+      <h1 className="center h_page">My Projects</h1>
+      <div className="card_container">
+        {projects_card.map(project=>(
+          <ProjectCard project={project}/>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Home
