3ab4b6a11ac6822da7345ff468043b46077eaa80
Pellegrino Piccolo <152791429+PellegrinoPiccolo@users.noreply.github.com>
2024-02-17 15:34:31 +0100

    card

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ function App() {
       <div className="top_section">
         <img src={Logo} alt="PellegrinoPiccolo"/>
         <div className="text_section">
-          <h2>THIS IS ONLY MY PROJECTS COLLECTION</h2>
-          <h2>IF YOU WANT VISIT MY PORTFOLIO YOU CAN CLICK ON <a href="https://pellegrinopiccolo.github.io/" target="_blank">THIS LINK (pellegrinopiccolo.github.io)</a></h2>
+          <h2 className="h_page">THIS IS ONLY MY PROJECTS COLLECTION</h2>
+          <h2 className="h_page">IF YOU WANT VISIT MY PORTFOLIO YOU CAN CLICK ON <a href="https://pellegrinopiccolo.github.io/" target="_blank">THIS LINK (pellegrinopiccolo.github.io)</a></h2>
         </div>
       </div>
-      <h1 className="center">My Projects</h1>
+      <h1 className="center h_page">My Projects</h1>
       <div className="card_container">
         {projects_card.map(project=>(
           <ProjectCard project={project}/>
--- src/components/projectCard/ProjectCard.jsx
+++ src/components/projectCard/ProjectCard.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 import './projectcard.scss'
 
 const ProjectCard = ({project}) => {
+
+  const maxLength = 80;
+
+  const description = project.description
+
   return (
     <div className='project_card'>
-      <div className="left">
+      <div className="image_container">
         <img src={project.img} alt="" />
+
       </div>
-      <div className="right">
-        <p className='title'>{project.name}</p>
-        <p className='description'>{project.description}</p>
-        <p>Link: {project.link ? (<a href={project.link} target="_blank" rel="noopener noreferrer">{project.link}</a>) : ('undefined')}</p>
+      <div className="text_container">
+        <p className='title poppins-bold'>{project.name}</p>
+        <p className='description poppins-regular'>{description.length > maxLength ? description.slice(0, maxLength) + "..." : description}</p>
+        <p className='link poppins-bold'>Link: {project.link ? (<a href={project.link} target="_blank" rel="noopener noreferrer">{project.link}</a>) : ('undefined')}</p>
         <button>Read More</button>
       </div>
     </div>
